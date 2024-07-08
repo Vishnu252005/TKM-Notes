065e17115224f3df08d6086cdb4e8324aea0e358
Vishnu S <141705372+Vishnu252005@users.noreply.github.com>
2024-07-09 00:08:01 +0530

    Add files via upload

---
+++ lib/EC/sem8/ec_sem8_screen.dart
@@ -0,0 +1,276 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_2/EC/sem8/units.dart';  // Import the correct file for units
+import 'package:flutter_application_2/widgets/profile.dart'; // Import the profile.dart file
+
+class ECSem8Screen extends StatefulWidget {
+  final String fullName;
+  final String branch;
+  final String year;
+  final String semester;
+
+  const ECSem8Screen({
+    Key? key,
+    required this.fullName,
+    required this.branch,
+    required this.year,
+    required this.semester,
+  }) : super(key: key);
+
+  @override
+  _ECSem8ScreenState createState() => _ECSem8ScreenState();
+}
+
+class _ECSem8ScreenState extends State<ECSem8Screen> {
+  int _selectedIndex = 0;
+  final List<String> _tabs = ['Notes & Books', 'PYQs'];
+
+  late Map<String, List<Map<String, dynamic>>> _subjects;
+
+  @override
+  void initState() {
+  super.initState();
+
+  _subjects = {
+    'Notes & Books': [
+      {
+        'name': 'Calculus and Linear Algebra',
+        'description': 'Study of calculus and linear algebra including...',
+        'image': 'assets/s8.png',
+        'page': () => ComputerNetworksPage(fullName: widget.fullName),
+      },
+      {
+        'name': 'Engineering Chemistry',
+        'description': 'Exploration of fundamental concepts in chemistry...',
+        'image': 'assets/s8.png',
+        'page': () => ComputerNetworksPage(fullName: widget.fullName),
+      },
+      {
+        'name': 'Fundamentals of Electronics Engineering',
+        'description': 'Basics of electronics and electrical engineering...',
+        'image': 'assets/s8.png',
+        'page': () => ComputerNetworksPage(fullName: widget.fullName),
+      },
+      {
+        'name': 'Engineering Graphics',
+        'description': 'Fundamentals of engineering drawing and graphics...',
+        'image': 'assets/s8.png',
+        'page': () => ComputerNetworksPage(fullName: widget.fullName),
+      },
+      {
+        'name': 'Manufacturing Practices',
+        'description': 'Introduction to various manufacturing processes...',
+        'image': 'assets/s8.png',
+        'page': () => ComputerNetworksPage(fullName: widget.fullName),
+      },
+      {
+        'name': 'Sports and Yoga',
+        'description': 'Physical education and well-being through sports and yoga...',
+        'image': 'assets/s8.png',
+        'page': () => ComputerNetworksPage(fullName: widget.fullName),
+      },
+      {
+        'name': 'Basics of Electrical Engineering',
+        'description': 'Introduction to electrical engineering principles...',
+        'image': 'assets/s8.png',
+        'page': () => ComputerNetworksPage(fullName: widget.fullName),
+      },
+      {
+        'name': 'Computer Aided Drafting and Modelling',
+        'description': 'Basics of computer-aided drafting and modelling...',
+        'image': 'assets/s8.png',
+        'page': () => ComputerNetworksPage(fullName: widget.fullName),
+      },
+    ],
+    'PYQs': [
+      {
+        'name': 'Calculus and Linear Algebra PYQs',
+        'description': 'Previous Year Questions for Calculus and Linear Algebra...',
+        'image': 'assets/s2.png',
+        'page': () => ComputerNetworksPage(fullName: widget.fullName),
+      },
+      {
+        'name': 'Engineering Chemistry PYQs',
+        'description': 'Previous Year Questions for Engineering Chemistry...',
+        'image': 'assets/s2.png',
+        'page': () => ComputerNetworksPage(fullName: widget.fullName),
+      },
+      {
+        'name': 'Fundamentals of Electronics Engineering PYQs',
+        'description': 'Previous Year Questions for Fundamentals of Electronics Engineering...',
+        'image': 'assets/s2.png',
+        'page': () => ComputerNetworksPage(fullName: widget.fullName),
+      },
+      {
+        'name': 'Engineering Graphics PYQs',
+        'description': 'Previous Year Questions for Engineering Graphics...',
+        'image': 'assets/s2.png',
+        'page': () => ComputerNetworksPage(fullName: widget.fullName),
+      },
+      {
+        'name': 'Manufacturing Practices PYQs',
+        'description': 'Previous Year Questions for Manufacturing Practices...',
+        'image': 'assets/s2.png',
+        'page': () => ComputerNetworksPage(fullName: widget.fullName),
+      },
+      {
+        'name': 'Sports and Yoga PYQs',
+        'description': 'Previous Year Questions for Sports and Yoga...',
+        'image': 'assets/s2.png',
+        'page': () => ComputerNetworksPage(fullName: widget.fullName),
+      },
+      {
+        'name': 'Basics of Electrical Engineering PYQs',
+        'description': 'Previous Year Questions for Basics of Electrical Engineering...',
+        'image': 'assets/s2.png',
+        'page': () => ComputerNetworksPage(fullName: widget.fullName),
+      },
+      {
+        'name': 'Computer Aided Drafting and Modelling PYQs',
+        'description': 'Previous Year Questions for Computer Aided Drafting and Modelling...',
+        'image': 'assets/s2.png',
+        'page': () => ComputerNetworksPage(fullName: widget.fullName),
+      },
+    ],
+  };
+}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 3, 83, 848),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Hey ${widget.fullName}',
+                        style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
+                      ),
+                      const Text(
+                        'Select Subject',
+                        style: TextStyle(fontSize: 86, color: Colors.white70),
+                      ),
+                    ],
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ProfilePage(
+                            fullName: widget.fullName,
+                            branch: widget.branch,
+                            year: widget.year,
+                            semester: widget.semester,
+                          ),
+                        ),
+                      );
+                    },
+                    child: CircleAvatar(
+                      backgroundColor: Colors.red[600],
+                      radius: 30,
+                      child: Text(
+                        widget.fullName[0].toUpperCase(),
+                        style: const TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 86),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(25),
+                    topRight: Radius.circular(25),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 86.0, vertical: 22),
+                      child: Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 58, 58, 58),
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: List.generate(
+                            _tabs.length,
+                            (index) => Expanded(
+                              child: GestureDetector(
+                                onTap: () => setState(() => _selectedIndex = index),
+                                child: Container(
+                                  padding: const EdgeInsets.symmetric(vertical: 82),
+                                  decoration: BoxDecoration(
+                                    color: _selectedIndex == index
+                                        ? Colors.black
+                                        : const Color.fromARGB(255, 58, 58, 58),
+                                    borderRadius: BorderRadius.circular(25),
+                                  ),
+                                  child: Text(
+                                    _tabs[index],
+                                    textAlign: TextAlign.center,
+                                    style: const TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Expanded(
+                      child: ListView.builder(
+                        padding: const EdgeInsets.symmetric(horizontal: 86),
+                        itemCount: _subjects[_tabs[_selectedIndex]]!.length,
+                        itemBuilder: (context, index) {
+                          var subject = _subjects[_tabs[_selectedIndex]]![index];
+                          return Card(
+                            color: const Color.fromARGB(255, 58, 58, 58),
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(82)),
+                            child: ListTile(
+                              contentPadding: const EdgeInsets.all(8),
+                              leading: subject['image'] != null
+                                  ? Image.asset(subject['image'], width: 80, height: 80)
+                                  : null,
+                              title: Text(subject['name'], style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+                              subtitle: Text(subject['description'], style: const TextStyle(color: Colors.white70)),
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => subject['page'](),
+                                  ),
+                                );
+                              },
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
