ceac11be983c427c7893be29889cf21def95d3d3
Vishnu252005 <vishnu1252005@gmail.com>
2024-09-15 01:30:27 +0530

    update

--- lib/widgets/profiledark.dart
+++ lib/widgets/profiledark.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 import '../widgets/sgpa.dart';
 import '../widgets/signup.dart';
 import '../widgets/calcu.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../widgets/syllabus.dart';
 import 'package:Nexia/widgets/pdfviewer.dart';
- 
+
+String? _examTimetableLink;
 
 class ProfilePage extends StatefulWidget {
   final String fullName;
@@ -14,6 +16,7 @@ class ProfilePage extends StatefulWidget {
   final String semester;
   final bool isDarkMode;
   final Function(bool) onThemeChanged;
+  
 
   ProfilePage({
     required this.fullName,
@@ -27,7 +30,13 @@ class ProfilePage extends StatefulWidget {
   @override
   _ProfilePageState createState() => _ProfilePageState();
 }
-
+void _launchURL(String url) async {
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
 class _ProfilePageState extends State<ProfilePage> {
   late bool _isDarkMode;
   late String _syllabusLink;
@@ -75,6 +84,159 @@ class _ProfilePageState extends State<ProfilePage> {
       context,
       MaterialPageRoute(builder: (context) => SignUpScreen()),
       (Route<dynamic> route) => false,
+    );
+  }
+   void _showAboutUsPopup() {
+    showModalBottomSheet(
+      context: context,
+      backgroundColor: Colors.transparent,
+      builder: (BuildContext context) {
+        return Container(
+          decoration: BoxDecoration(
+            color: _isDarkMode ? Color(0xFF202020) : Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Nexia Notes 🎓',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: _isDarkMode ? Colors.white : Colors.black,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Nexia Notes comes with all possible materials an engineer has been looking for.',
+                  style: TextStyle(color: _isDarkMode ? Colors.white70 : Colors.black87),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'We found people struggling for resources, and spending hours on getting basic material, Nexia Notes solves your problem by providing everything in a single place with an easy and ad-free experience.',
+                  style: TextStyle(color: _isDarkMode ? Colors.white70 : Colors.black87),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Nexia Notes comes with:',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: _isDarkMode ? Colors.white : Colors.black,
+                  ),
+                ),
+                SizedBox(height: 5),
+                Text(
+                  '- All types of notes for all possible branches\n'
+                  '- Updated PYQ\'s\n'
+                  '- Easiness to find the syllabus\n'
+                  '- Updated time-table for university examinations\n'
+                  '- Useful tools and notification updates',
+                  style: TextStyle(color: _isDarkMode ? Colors.white70 : Colors.black87),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Developed By:',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: _isDarkMode ? Colors.white : Colors.black,
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () => _launchURL('https://www.instagram.com/_vishnu._.25/'),
+                  child: Text(
+                    'Vishnu S',
+                    style: TextStyle(color: _isDarkMode ? Colors.blue : Colors.blue),
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  void _showSupportUsPopup() {
+    showModalBottomSheet(
+      context: context,
+      backgroundColor: Colors.transparent,
+      builder: (BuildContext context) {
+        return Container(
+          decoration: BoxDecoration(
+            color: _isDarkMode ? Color(0xFF202020) : Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'FOUND SOME NOTES MISSING? 🤔',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: _isDarkMode ? Colors.white : Colors.black,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Support us by submitting your materials using the form provided below.',
+                  style: TextStyle(color: _isDarkMode ? Colors.white70 : Colors.black87),
+                ),
+                SizedBox(height: 15),
+                ElevatedButton(
+                  child: Text(
+                    'Submit Materials', style: TextStyle(  color:  Colors.white ),),
+                  onPressed: () {
+                    _launchURL("https://forms.gle/ze5DsM2hZj98ubWw9");
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    minimumSize: Size(double.infinity, 50),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'SUPPORT US TO GROW 🚀',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: _isDarkMode ? Colors.white : Colors.black,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Finding app useful? Support us by sharing it with your friends or rate us on playstore.',
+                  style: TextStyle(color: _isDarkMode ? Colors.white70 : Colors.black87),
+                ),
+                SizedBox(height: 15),
+                ElevatedButton(
+                  child: Text('Rate Us Now' , style: TextStyle(  color:  Colors.white ),),
+                  onPressed: () {
+                    _launchURL("https://forms.gle/WnnFQRTkWjYHxfAv5");
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    minimumSize: Size(double.infinity, 50),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 
@@ -141,12 +303,20 @@ class _ProfilePageState extends State<ProfilePage> {
                       onTap: _launchPDFViewer,
                     ),
                     // navigate to syllabus page
+                    
+                    
                     ListTile(
                       leading: Icon(Icons.calendar_today, color: _isDarkMode ? Colors.white : Colors.black),
                       title: Text('Exam Timetable', style: TextStyle(color: _isDarkMode ? Colors.white : Colors.black)),
                       trailing: Icon(Icons.chevron_right, color: _isDarkMode ? Colors.white : Colors.black),
                       onTap: () {
-                        // Navigate to Exam Timetable page
+                        if (_examTimetableLink == null || _examTimetableLink!.isEmpty) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text('No exams dude, just be chill!')),
+                          );
+                        } else {
+                          _launchURL(_examTimetableLink!);
+                        }
                       },
                     ),
                     ListTile(
@@ -179,25 +349,19 @@ class _ProfilePageState extends State<ProfilePage> {
                 leading: Icon(Icons.language, color: _isDarkMode ? Colors.white : Colors.black),
                 title: Text('Website', style: TextStyle(color: _isDarkMode ? Colors.white : Colors.black)),
                 trailing: Icon(Icons.chevron_right, color: _isDarkMode ? Colors.white : Colors.black),
-                onTap: () {
-                  // Navigate to Website page
-                },
+                onTap: () => _launchURL('https://nexianotes.vercel.app/'),
               ),
               ListTile(
                 leading: Icon(Icons.info, color: _isDarkMode ? Colors.white : Colors.black),
                 title: Text('About Us', style: TextStyle(color: _isDarkMode ? Colors.white : Colors.black)),
                 trailing: Icon(Icons.chevron_right, color: _isDarkMode ? Colors.white : Colors.black),
-                onTap: () {
-                  // Navigate to About Us page
-                },
+                onTap: _showAboutUsPopup,
               ),
               ListTile(
                 leading: Icon(Icons.support, color: _isDarkMode ? Colors.white : Colors.black),
                 title: Text('Support Us', style: TextStyle(color: _isDarkMode ? Colors.white : Colors.black)),
                 trailing: Icon(Icons.chevron_right, color: _isDarkMode ? Colors.white : Colors.black),
-                onTap: () {
-                  // Navigate to Support Us page
-                },
+                onTap: _showSupportUsPopup,
               ),
               ListTile(
                 leading: Icon(Icons.share, color: _isDarkMode ? Colors.white : Colors.black),
